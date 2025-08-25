d185df3e70c4bb0c851bba842876dcaa91756119
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-08-25 14:02:38 +0530

    Add logout page and middlewares

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,61 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import {toast} from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
-function page() {
+
+export default function UserProfilePage() {
+
+  const [data ,setData] = useState("nothing");
+
+  const router = useRouter();
+
+  const getUserDetails = async() => {
+    try {
+      const response = await axios.post("/api/users/AboutMe")
+      console.log(response.data.data._id)
+      setData(response.data.data._id);
+
+    } catch ( error : any) {
+      console.log(error.message)
+      toast.error(error.message)
+    }
+  }
+
+  const Logout = async() => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login")
+
+    } catch (error : any) {
+      console.log(error.message);
+      toast.error(error.message)
+      
+    }
+  }
+
   return (
-    <div>page</div>
+    <div className='flex flex-col justify-center items-center m-5 py-2 min-h-screen'>
+      <h1 className='font-bold text-3xl font-sans mb-4'>Profile</h1>
+      <hr/>
+      
+      <div className='flex flex-col r items-center p-4 border-2 rounded-2xl border-white bg-transparent border-r-2'>
+      <h2 className='mb-4'>Profile Page</h2>
+      <h2>{data === "nothing" ? "Nothing" : <Link href = {`/profile/${data}`}>{data}</Link>}</h2>
+      <hr/>
+
+      <button
+      onClick={Logout}
+      className='bg-blue-400 font-semibold border-2 px-4 py-2 rounded-2xl m-4'> logout </button>
+
+      <button
+      onClick={getUserDetails}
+      className='bg-green-400 font-semibold border-2 px-4 py-2 rounded-2xl m-4'>Get User Details</button>
+    </div>
+    </div>
   )
 }
-
-export default page
--- src/helper/getDataFromToken.ts
+++ src/helper/getDataFromToken.ts
@@ -1,20 +1,14 @@
-import { request } from "http"
 import { NextRequest } from "next/server";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
+export const getDataFromToken = (request: NextRequest) => {
+  try {
+    const token = request.cookies.get("token")?.value;
+    if (!token) throw new Error("Token not found");
 
-export const getDataFromToken = (request : NextRequest) => {
-    try {
-        // request.cookies.get("token") → tries to read a cookie named "token".
-        // ?.value → optional chaining; if cookie exists, take its value, else undefined.
-        // || '' → if no cookie, fallback to an empty string.
-        // At this point, token contains the JWT string (if present).
-        const token = request.cookies.get("token")?.value || "";
-        const decodedToken : any = jwt.verify(token , process.env.TOKEN_SECRET!)
-
-        return decodedToken.id;
-
-    } catch (error : any) {
-        throw new Error(error.message)
-    }
-}
+    const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+    return decodedToken.id;
+  } catch (error: any) {
+    throw new Error(error.message || "Invalid token");
+  }
+};
